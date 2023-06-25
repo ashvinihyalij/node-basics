abc7abf5e329a04ff5b41cd475ac2dfa3d6c2786
ashvinihyalij <ashvinihyalij@gmail.com>
2023-06-25 22:43:27 +0530

    product edit functionality

--- routes/admin.js
+++ routes/admin.js
@@ -16,4 +16,6 @@ router.post('/add-product', adminController.postAddProduct);
 // /admin/products => POST
 router.get('/products', adminController.getProducts);
 
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 module.exports = router;
